e07e2469584640a238e089710603690ef1f7aead
shyumAhmed <shyumahmed@gmail.com>
2022-06-16 23:22:31 -0400

    wrote draw function where it uses mouse event listners to draw on the sketch board. only in black color for now.

--- javascript/script.js
+++ javascript/script.js
@@ -11,4 +11,25 @@ function createGrid(numOfRows, numOfCells) {
   }
 }
 
-createGrid(50, 50);
+function draw() {
+  const cells = document.querySelectorAll(".cell");
+  let drag = false;
+  cells.forEach((cell) => {
+    cell.addEventListener("mousedown", (e) => {
+      drag = true;
+      e.target.style.background = "black";
+      console.log("mousedown");
+    });
+    cell.addEventListener("mouseover", (e) => {
+      drag ? (e.target.style.background = "black") : "";
+    });
+    cell.addEventListener("mouseup", () => {
+      console.log("mouseup");
+      drag = false;
+    });
+  });
+}
+
+createGrid(16, 16);
+draw();
+//(e.target.style.background = " antiquewhite")
