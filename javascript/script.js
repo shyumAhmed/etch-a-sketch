da2e83d20e0b30e25ed7adc0738afa411abd6b83
shyumAhmed <shyumahmed@gmail.com>
2022-06-18 00:27:40 -0400

    added shading and rainbow functionality

--- javascript/script.js
+++ javascript/script.js
@@ -1,13 +1,20 @@
 const sketchBox = document.querySelector(".sketch-box");
 const clearButton = document.querySelector(".clear");
 const toggleGridButton = document.querySelector(".toggle-lines");
+const toggleRainbowButton = document.querySelector(".toggle-rainbow");
+const toggleColorButton = document.querySelector(".toggle-color");
+const toggleShadowButton = document.querySelector(".toggle-shadow");
 const slider = document.getElementById("myRange");
 const sliderValueDisp = document.getElementById("value-num");
 const colorPicker = document.getElementById("colorpicker");
-let cellColor = "black";
+let cellColor;
+let isRainbow = false;
+let isColor = true;
+let isShadow = false;
+let solidColor = "black";
+let shadePercentage = 10;
 
 function createDefaultGrid(numOfRows, numOfCells) {
-  console.log("being called");
   for (r = 0; r < numOfRows; r++) {
     let row = document.createElement("div");
     sketchBox.appendChild(row).className = "grid-row";
@@ -22,12 +29,18 @@ function drawOnGrid() {
   const cells = document.querySelectorAll(".cell");
   let drag = false;
   cells.forEach((cell) => {
+    colorPicker.oninput = () => {
+      solidColor = colorPicker.value;
+      cellColor = solidColor;
+    };
     cell.addEventListener("mousedown", (e) => {
       drag = true;
+      updateColor(e, drag);
       e.target.style.background = cellColor;
       console.log("mousedown");
     });
     cell.addEventListener("mouseover", (e) => {
+      updateColor(e, drag);
       drag ? (e.target.style.background = cellColor) : "";
     });
     cell.addEventListener("mouseup", () => {
@@ -37,11 +50,25 @@ function drawOnGrid() {
   });
 }
 
+function updateColor(e, drag) {
+  if (isRainbow) {
+    cellColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  } else if (isColor) {
+    colorPicker.oninput = () => {
+      solidColor = colorPicker.value;
+    };
+    cellColor = solidColor;
+  } else if (isShadow) {
+    drag ? (e.target.style.filter = "brightness(30%)") : "";
+  }
+}
+
 function clearBoard() {
   const cells = document.querySelectorAll(".cell");
   clearButton.addEventListener("click", () => {
     cells.forEach((cell) => {
       cell.style.background = "antiquewhite";
+      cell.style.filter = "brightness(100%)";
     });
   });
 }
@@ -63,6 +90,8 @@ function updateGridCellNum() {
     clearBoard();
     toggleGridLines();
     changeCellColor();
+    changeToRandomColor();
+    changeCellShading();
   };
 }
 
@@ -73,6 +102,8 @@ function createGrid() {
   clearBoard();
   toggleGridLines();
   changeCellColor();
+  changeToRandomColor();
+  changeCellShading();
   updateGridCellNum();
 }
 
@@ -95,9 +126,30 @@ function toggleGridLines() {
 }
 
 function changeCellColor() {
-  colorPicker.oninput = () => {
-    cellColor = colorPicker.value;
-  };
+  toggleColorButton.addEventListener("click", () => {
+    isColor = true;
+    isRainbow = false;
+    isShadow = false;
+    console.log("sssss");
+  });
 }
+
+function changeCellShading() {
+  toggleShadowButton.addEventListener("click", () => {
+    isRainbow = false;
+    isColor = false;
+    isShadow = true;
+  });
+}
+
+function changeToRandomColor() {
+  toggleRainbowButton.addEventListener("click", () => {
+    isRainbow = true;
+    isColor = false;
+    isShadow = false;
+  });
+}
+
+function configureGrid() {}
 
 createGrid();
